0b839d366f32ef447a4f78de01195dbb5f7635c0
Artur Kvasnikov <artcshq@gmail.com>
2019-06-18 16:24:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+console.log(a);
+
+let person = {
+    name: 'John',
+    age: 25,
+    isMarried:false
+};
+
+
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+
+ 
+  let awnswer = +prompt("Are you here?");
+//  console.log(awnswer);
+console.log(typeof(awnswer));
+
+ console.log(4+'-object');
